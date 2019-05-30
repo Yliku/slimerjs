a45aec21f14a9d9024f8aef981147d76be5de7fe
Yliku <370495310@qq.com>
2019-05-30 11:41:53 +0800

    修改 image.js文件；添加 .gitignore

--- js/image.js
+++ js/image.js
@@ -9,6 +9,7 @@ webpage.open(url, function (status) {
     // webpage.viewportSize = { width:widthSize, height:heightSize };
     slimer.wait(waitTime);
     var dt = new Date();	//js语法
-    webpage.render('./snapshotImage/'+ picName + dt.getTime()+'.png');
+    webpage.render('./public/snapshotImage/'+ picName + dt.getTime()+'.png');
+    //文件保存的相对路径是相对于执行该命令的窗口的路径，在laravel中也就是能执行 artisan命令，即是laravel项目的路径
     slimer.exit();
 });
