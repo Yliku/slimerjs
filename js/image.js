097b7c7d5de993ebc5f792733e4cb9c4f1d1cecd
Yliku <370495310@qq.com>
2019-06-12 15:37:52 +0800

    调整代码

--- js/image.js
+++ js/image.js
@@ -1,15 +1,16 @@
 var webpage = require('webpage').create();
 var url = phantom.args[0];		//命令行的第一个参数
-var widthSize = phantom.args[1];
-var heightSize = phantom.args[2];
-var waitTime = phantom.args[3];
-var picName = phantom.args[4];
+var waitTime = phantom.args[1];
+var picName = phantom.args[2];
+var savePath = phantom.args[3];
+// var widthSize = phantom.args[3];
+// var heightSize = phantom.args[4];
 
 webpage.open(url, function (status) {
-    // webpage.viewportSize = { width:widthSize, height:heightSize };
+    // webpage.viewportSize = { width:widthSize, height:heightSize };	//截图窗口预览的大小
     slimer.wait(waitTime);
     var dt = new Date();	//js语法
-    webpage.render('./public/snapshotImage/'+ picName + dt.getTime()+'.png');
+    webpage.render(savePath + '/'+ dt.getTime() + picName + '.png');
     //文件保存的相对路径是相对于执行该命令的窗口的路径，在laravel中也就是能执行 artisan命令，即是laravel项目的路径
     slimer.exit();
 });
